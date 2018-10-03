1c64826af2b221212a8f3cf308a6ebfb01e30d7a
nickly <lyubimov.nkt@gmail.com>
2018-10-04 02:20:56 +0300

    kotlin-spring base project

---
+++ src/test/kotlin/com/gas/transport/visualisation/GasTransportVisualisationApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.gas.transport.visualisation
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class GasTransportVisualisationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
