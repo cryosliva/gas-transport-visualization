30267da8e21b04a2a3884f8f387b967784ea4927
Nikita Lyubimov <lyubimov.nkt@gmail.com>
2019-01-12 20:53:49 +0300

    dev profile for data source

--- src/main/kotlin/com/gas/transport/visualisation/config/JpaConfig.kt
+++ src/main/kotlin/com/gas/transport/visualisation/config/JpaConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import javax.sql.DataSource
 
@@ -12,6 +13,7 @@ import javax.sql.DataSource
 @EntityScan(basePackages = ["com.gas.transport.visualisation.model.entity"])
 class JpaConfig {
     @Bean
+    @Profile("dev")
     fun devDataSource(): DataSource =
             DataSourceBuilder.create()
                     .username("postgres")
