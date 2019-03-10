524d4cf5dbafdf45c325ff37197dd7b8ba82ad94
Nick Lyubimov <lyubimov.nkt@gmail.com>
2019-03-10 18:05:13 +0300

    Heroku (#13)
    Heroku deploy

--- src/main/kotlin/com/gas/transport/visualisation/config/JpaConfig.kt
+++ src/main/kotlin/com/gas/transport/visualisation/config/JpaConfig.kt
@@ -21,4 +21,14 @@ class JpaConfig {
                     .driverClassName("org.postgresql.Driver")
                     .url("jdbc:postgresql://localhost:5432/gtv")
                     .build()
+
+    @Bean
+    @Profile("prod")
+    fun prodDataSource(): DataSource =
+            DataSourceBuilder.create()
+                    .username("uvstxzqprhqcyl")
+                    .password("a95e971bac8f9dc0473ba04663fefd89d334befd06a6c3e7c4ec23c14612e329")
+                    .driverClassName("org.postgresql.Driver")
+                    .url("jdbc:postgresql://ec2-54-247-85-251.eu-west-1.compute.amazonaws.com:5432/d6uptcffdhnorv")
+                    .build()
 }
