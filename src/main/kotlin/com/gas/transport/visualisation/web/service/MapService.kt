e72f8e5acecc79b3aa02cb2809d523ba32ba9602
Nikita Lyubimov <lyubimov.nkt@gmail.com>
2019-03-11 00:13:13 +0300

    map controller added #16

--- src/main/kotlin/com/gas/transport/visualisation/web/service/MapService.kt
+++ src/main/kotlin/com/gas/transport/visualisation/web/service/MapService.kt
@@ -13,28 +13,21 @@ import org.springframework.stereotype.Service
 class MapService {
 
     @Autowired
-    private lateinit var nodeCustomDao: NodeCustomDao
+    private lateinit var nodeService: NodeService
 
-    fun getMap(filter: NodeFilterDto) : MapDto{
-        var specification = hasNodeYear(filter.year).and(hasNodeSnapshot(filter.snapshotId))
-        if (filter.region != null)
-            specification = specification.and(hasRegion(filter.region))
-
-        if (filter.type != null)
-            specification = specification.and(hasNodeType(filter.type))
-
-        val nodes = nodeCustomDao.findAll(specification)
+    fun getMap(filter: NodeFilterDto): MapDto {
+        val nodes = nodeService.getUnpreparedFilteredNodes(filter)
         val pipes = mutableListOf<Pipe>()
         val unrelatedPipes = mutableListOf<Pipe>()
-        nodes.forEach {node->
-            node.demandsSources.forEach {pipe->
-                if(nodes.contains(pipe.source) && nodes.contains(pipe.destination))
+        nodes.forEach { node ->
+            node.demandsSources.forEach { pipe ->
+                if (nodes.contains(pipe.source) && nodes.contains(pipe.destination))
                     pipes.add(pipe)
                 else
                     unrelatedPipes.add(pipe)
             }
         }
 
-        return MapDto(nodes.map { node->node.toNodeDto() }, pipes.distinct().map { pipe->pipe.toPipeDto() }, unrelatedPipes.map { pipe->pipe.toPipeDto() } )
+        return MapDto(nodes.map { node -> node.toNodeDto() }, pipes.distinct().map { pipe -> pipe.toPipeDto() }, unrelatedPipes.map { pipe -> pipe.toPipeDto() })
     }
 }
