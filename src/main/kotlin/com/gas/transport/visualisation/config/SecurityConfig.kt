718ef40aac5d73963a05c3b4df227b6fa686748d
Nikita Lyubimov <lyubimov.nkt@gmail.com>
2019-03-19 13:37:25 +0300

    cors fix

--- src/main/kotlin/com/gas/transport/visualisation/config/SecurityConfig.kt
+++ src/main/kotlin/com/gas/transport/visualisation/config/SecurityConfig.kt
@@ -10,6 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
 
 @Configuration
 @EnableWebSecurity
@@ -36,4 +41,15 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     @Bean
     fun encoder() =  BCryptPasswordEncoder()
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurerAdapter() {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+            }
+
+        }
+    }
 }
